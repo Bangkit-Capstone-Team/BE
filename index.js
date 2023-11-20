08fc86ef0f02438d18fb9b4dde82c8e1a76e9168
windisiswandi <windisiswandi89@gmail.com>
2023-11-20 10:18:59 +0800

    feat: add cookie-parser

--- index.js
+++ index.js
@@ -3,15 +3,19 @@ const express = require("express")
 const app = express()
 const routes = require("./config/routes")
 const bodyParser = require("body-parser")
+const cookieParser = require('cookie-parser')
 const PORT = process.env.PORT || 8080
 
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(cookieParser())
+app.use(express.json())
+// app.use(bodyParser.urlencoded({extended: false}))
 
 routes(app)
 
 app.use((req, res) => {
-    res.statusCode = 404
-    res.send("Not Found")
+    // res.statusCode = 404
+    // res.send("Not Found")
+    res.send(req.playe)
 })
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`))
