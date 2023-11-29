38d8f50c4cab8598a151f5df4d3b43e2c11a3a28
windisiswandi <windisiswandi89@gmail.com>
2023-11-29 13:42:03 +0800

    update route index.js

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const bodyParser = require("body-parser")
 const cors = require('cors');
 const PORT = process.env.APP_PORT || 5000
 
-app.use(bodyParser.json())
 app.use(cors());
+app.use(bodyParser.json())
+
 routes(app)
 
 app.use(express.json())
-app.use(routes)
 
 app.use((req, res) => {
     res.statusCode = 404
