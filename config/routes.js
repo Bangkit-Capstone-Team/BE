f700588d7ce8369d0ac699991544d7631c2e09b5
windisiswandi <windisiswandi89@gmail.com>
2023-11-29 15:14:59 +0800

    fix: url endpoint

--- config/routes.js
+++ config/routes.js
@@ -63,52 +63,52 @@ const base_url = '/api/v1'
 module.exports = (app) => {
   //user
   app.get(`${base_url}/users`, getUsers);
-  app.get(`${base_url}/user/:id`, getUserById);
-  app.post(`${base_url}/user/add`, insertUser);
-  app.put(`${base_url}/user/:id`, updateDataUser);
-  app.delete(`${base_url}/user/:id`, deleteUser);
+  app.get(`${base_url}/users/:id`, getUserById);
+  app.post(`${base_url}/users`, insertUser);
+  app.put(`${base_url}/users/:id`, updateDataUser);
+  app.delete(`${base_url}/users/:id`, deleteUser);
 
   //review
-  app.get(`${base_url}/review`, getReviews);
-  app.get(`${base_url}/review/:id`, getReviewById);
-  app.post(`${base_url}/review/`, insertReview);
-  app.put(`${base_url}/review/:id`, updateDataReview);
-  app.delete(`${base_url}/review/:id`, deleteReview);
+  app.get(`${base_url}/reviews`, getReviews);
+  app.get(`${base_url}/reviews/:id`, getReviewById);
+  app.post(`${base_url}/reviews`, insertReview);
+  app.put(`${base_url}/reviews/:id`, updateDataReview);
+  app.delete(`${base_url}/reviews/:id`, deleteReview);
 
   //categories
   app.get(`${base_url}/categories`, getCategories);
-  app.get(`${base_url}/category/:id`, getCategoryById);
-  app.post(`${base_url}/category/add`, insertCategory);
-  app.put(`${base_url}/category/:id`, updateCategory);
-  app.delete(`${base_url}/category/:id`, deleteCategory);
+  app.get(`${base_url}/categories/:id`, getCategoryById);
+  app.post(`${base_url}/categories`, insertCategory);
+  app.put(`${base_url}/categories/:id`, updateCategory);
+  app.delete(`${base_url}/categories/:id`, deleteCategory);
 
   //has_category
   app.get(`${base_url}/has-categories`, getHasCategories);
-  app.get(`${base_url}/has-category/:id`, getHasCategoryById);
-  app.post(`${base_url}/has-category/add`, insertHasCategory);
-  app.put(`${base_url}/has-category/:id`, updateDataHasCategory);
-  app.delete(`${base_url}/has-category/:id`, deleteHasCategory);
+  app.get(`${base_url}/has-categories/:id`, getHasCategoryById);
+  app.post(`${base_url}/has-categories`, insertHasCategory);
+  app.put(`${base_url}/has-categories/:id`, updateDataHasCategory);
+  app.delete(`${base_url}/has-categories/:id`, deleteHasCategory);
 
   //content
   app.get(`${base_url}/contents`, getContents);
-  app.get(`${base_url}/content/:id`, getContentById);
-  app.post(`${base_url}/content/add`, insertContent);
-  app.put(`${base_url}/content/:id`, updateContent);
-  app.delete(`${base_url}/content/:id`, deleteContent);
+  app.get(`${base_url}/contents/:id`, getContentById);
+  app.post(`${base_url}/contents`, insertContent);
+  app.put(`${base_url}/contents/:id`, updateContent);
+  app.delete(`${base_url}/contents/:id`, deleteContent);
 
   //content_view_logs
   app.get(`${base_url}/content-view-logs`, getContentViewLogs);
-  app.get(`${base_url}/content-view-log/:id`, getContentViewLogById);
-  app.post(`${base_url}/content-view-log/add`, insertContentViewLog);
-  app.put(`${base_url}/content-view-log/:id`, updateContentViewLog);
-  app.delete(`${base_url}/content-view-log/:id`, deleteContentViewLog);
+  app.get(`${base_url}/content-view-logs/:id`, getContentViewLogById);
+  app.post(`${base_url}/content-view-logs`, insertContentViewLog);
+  app.put(`${base_url}/content-view-logs/:id`, updateContentViewLog);
+  app.delete(`${base_url}/content-view-logs/:id`, deleteContentViewLog);
 
   //content_reaction_logs
   app.get(`${base_url}/content-reaction-logs`, getContentReactionLogs);
-  app.get(`${base_url}/content-reaction-log/:id`, getContentReactionLogById);
-  app.post(`${base_url}/content-reaction-log/add`, insertContentReactionLog);
-  app.put(`${base_url}/content-reaction-log/:id`, updateContentReactionLog);
-  app.delete(`${base_url}/content-reaction-log/:id`, deleteContentReactionLog);
+  app.get(`${base_url}/content-reaction-logs/:id`, getContentReactionLogById);
+  app.post(`${base_url}/content-reaction-logs`, insertContentReactionLog);
+  app.put(`${base_url}/content-reaction-logs/:id`, updateContentReactionLog);
+  app.delete(`${base_url}/content-reaction-logs/:id`, deleteContentReactionLog);
 
   app.post(`${base_url}/login`, authLogin)
   app.get(`${base_url}/get-access-token`, (req, res) => {
