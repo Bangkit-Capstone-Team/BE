f8fc245c7c971fcb12409bdac984c7c1a55d2286
Ilham Zukhri <112614524+ilhamZ354@users.noreply.github.com>
2023-11-29 23:21:56 +0700

    reupdate routes, update migrations and model :view-log, restore banner

--- database/migrations/20231124184642-create-content-view-logs.js
+++ database/migrations/20231124184642-create-content-view-logs.js
@@ -18,6 +18,11 @@ module.exports = {
       source: {
         type: Sequelize.STRING(50)
       },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
     });
   },
   async down(queryInterface, Sequelize) {
