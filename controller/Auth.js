6bbfe3050658ca66f3e7001d482daee9de3fab8a
windisiswandi <windisiswandi89@gmail.com>
2023-12-01 23:28:33 +0800

    fix: auth.js

--- controller/Auth.js
+++ controller/Auth.js
@@ -1,25 +1,28 @@
 const {Users} = require('../database/models');
-const bcrypt = require('bcrypt')
+const { decrypt } = require('../helper/bcrypt');
 const jwt = require('jsonwebtoken')
 
 const authLogin = async (req, res) => {
     const { username, password } = req.body
 
-    try {
-        const userData = await Users.findAll({ where: { username } })
-        const match = await bcrypt.compare(password, userData[0].password)
-        if (!match) return res.status(400).json({ msg: "Password salah" })
-        
-        const {id, email, type_role} = userData[0]
-        const token = jwt.sign({ id, email, role: type_role }, process.env.TOKEN_SECRET, {expiresIn: '10s'});
-        // res.cookie('token', token, {
-        //     maxAge: 24 * 60 * 60 * 1000,
-        //     httpOnly: true
-        // })
-        res.json({token})
-    } catch (error) {
-        res.status(404).json({msg: "Username tidak ditemukan"})
-    }
+    Users
+        .findAll({ where: { username } })
+        .then(data => {
+            if (data.length == 0) return res.status(404).json({ msg: 'Username tidak ditemukan' });
+            const match = decrypt(password, data[0].password);
+            if (!match) return res.status(400).json({ msg: 'Password salah' });
+            const { id, email, type_role } = userData[0];
+            const access_token = jwt.sign({ id, email, role: type_role }, process.env.ACCESS_TOKEN, { expiresIn: '600s' });
+            const refresh_token = jwt.sign({ id, email, role: type_role }, process.env.ACCESS_TOKEN, { expiresIn: '600s' });
+
+            return res.json({ access_token, refresh_token });
+        })
+        .catch(err => {
+            console.log(err);
+            res.statusCode = 500;
+            res.send('Server Error');
+        })
+
 }
 
 module.exports = {authLogin}
