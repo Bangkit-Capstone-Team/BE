695432bc76ca8382301f3329b9e1c6fc7aab18a6
windisiswandi <windisiswandi89@gmail.com>
2023-12-14 14:53:21 +0800

    feat: add apidocs with swagger

--- index.js
+++ index.js
@@ -2,17 +2,16 @@ require("dotenv").config()
 const express = require("express")
 const app = express()
 const routes = require("./config/routes")
-const bodyParser = require("body-parser")
 const cors = require('cors');
 const PORT = process.env.APP_PORT || 5000
+const swaggerUI = require('swagger-ui-express')
+const apidocs = require('./apidocs.json')
 
 app.use(cors());
-app.use(bodyParser.json())
 
 routes(app)
 
-app.use(express.json())
-
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(apidocs))
 app.use((req, res) => {
     res.statusCode = 404
     res.send("Not Found")
